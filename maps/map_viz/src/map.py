964987356da3caac98c121b75ca35698b51cf4c8
Gorjan <gorjan@fareboom.com>
2021-11-07 13:25:47 +0100

    Final map

--- maps/map_viz/src/map.py
+++ maps/map_viz/src/map.py
@@ -1,26 +1,44 @@
 import ast
 import os
+from dateutil.relativedelta import relativedelta as rd
+from datetime import date, datetime, timedelta
 
+import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-from datetime import date, datetime, timedelta
+from scipy.special import softmax
 
 from . import DATA_DIR
 
 APP = dash.Dash(__name__)
 APP.layout = html.Div([
+        html.Div(
+            [
+                html.H5("Date"),
+                dcc.DatePickerSingle(
+                    id="Date",
+                    min_date_allowed=date(2017, 1, 1),
+                    max_date_allowed=date(2021, 8, 31),
+                    initial_visible_month=date(2020, 8, 31),
+                    date=date(2020, 8, 31)
+                ),
+            ],
+
+                style={'width': '10%',
+                    'display': 'inline-block', 'vertical-align': 'top'
+                    }),
     html.Div([
             html.H5("Waste class"),
             html.Div(
                 [
                     dcc.Dropdown(
                         id="waste_class",
-                        options=[{"label":"Glass", "value":"Glass"}, {"label":"Plastic", "value":"Plastic"}],
-                        value='Glass'),
+                        options = [{"label":"All", "value": "All"}] + [{"label": el, "value":el} for el in ['Baterije', 'Medicinski odpadki', 'Olje', 'Metalni odpadki', 'Nevarne snovi', 'Električni odpadki', 'Gradbeni odpadki', 'Plastika', 'Papir', 'Mešana embalaža', 'Les', 'Bio', 'Steklo']],
+                        value='All'),
                 ],
 
 
@@ -28,6 +46,7 @@ APP.layout = html.Div([
             )], style={'width': '15%',
                        'display': 'inline-block', 'vertical-align': 'top'
                        }),
+        
         html.Div([
             html.H5("City"),
             html.Div(
@@ -51,21 +70,17 @@ APP.layout = html.Div([
             )], style={'width': '15%',
                        'display': 'inline-block', 'vertical-align': 'top'
                        }),
-        html.Div(
-            [
-                html.H5("Date"),
-                dcc.DatePickerSingle(
-                    id="Date",
-                    min_date_allowed=date(2017, 1, 1),
-                    max_date_allowed=date(2021, 8, 31),
-                    initial_visible_month=date(2021, 8, 31),
-                    date=date(2021, 8, 31)
-                ),
-            ],
-
-                style={'width': '20%',
-                    'display': 'inline-block', 'vertical-align': 'top'
-                    }),
+    
+        html.Div([
+            dcc.Checklist(
+                id="is_green",
+                options=[{'label': "GreenHax?", 'value': "YES"}],
+                value=["YES"]),
+        ], 
+            style={
+                  'display': 'inline-block', 'vertical-align': 'top'
+                  }
+                  ),
         html.Div([
             html.Div(
                 [
@@ -82,12 +97,13 @@ APP.layout = html.Div([
             html.Div(
                 [
                     dcc.Slider(
+                        id='date_offset',
                         min=0,
                         max=30,
                         step=None,
                         marks={i:f"-{30-i-1}" for i in range(0, 30)},
                             
-                        value=0
+                        value=29
                         )
                 ],
 
@@ -97,23 +113,46 @@ APP.layout = html.Div([
 
     html.Div([
             dcc.Graph(id='map_graph')
-        ]
-                       ),
+        ]),
+    html.Div([
+            dcc.Graph(id='bar_graph')
+        ]),
 
 ])
 
 
-RAW_DF =  pd.read_csv(os.path.join(DATA_DIR, "locations.csv"), index_col=0)
+# RAW_DF =  pd.read_csv(os.path.join(DATA_DIR, "locations.csv"), index_col=0)
+
+RAW_DF_CLUS =  pd.read_csv(os.path.join(DATA_DIR, "EVL_boshko_done_DB-6700.csv"), index_col=0)
+ALL_LOCS_OD = RAW_DF_CLUS[['DB_CLUSTER_LOCATION_ODDAJA','DB_CLUSTER_LOCATION_ODDAJA_X', "DB_CLUSTER_LOCATION_ODDAJA_Y"]].drop_duplicates(subset=['DB_CLUSTER_LOCATION_ODDAJA']).sort_values(by='DB_CLUSTER_LOCATION_ODDAJA').reset_index(drop=True).rename({"DB_CLUSTER_LOCATION_ODDAJA":"cluster_id"}, axis=1)
+ALL_LOCS_DO = RAW_DF_CLUS[['DB_CLUSTER_LOCATION_PREJEMA','DB_CLUSTER_LOCATION_PREJEMA_X', "DB_CLUSTER_LOCATION_PREJEMA_Y"]].drop_duplicates(subset=['DB_CLUSTER_LOCATION_PREJEMA']).sort_values(by='DB_CLUSTER_LOCATION_PREJEMA').reset_index(drop=True).rename({"DB_CLUSTER_LOCATION_PREJEMA":"cluster_id"}, axis=1)
+ALL_LOCS = ALL_LOCS_OD.merge(ALL_LOCS_DO, how='outer', on='cluster_id')
+ALL_LOCS['X']=ALL_LOCS[['DB_CLUSTER_LOCATION_PREJEMA_X', 'DB_CLUSTER_LOCATION_ODDAJA_X']].max(axis=1)
+ALL_LOCS['Y']=ALL_LOCS[['DB_CLUSTER_LOCATION_PREJEMA_Y', 'DB_CLUSTER_LOCATION_ODDAJA_Y']].max(axis=1)
+ALL_LOCS = ALL_LOCS[['cluster_id', "X", "Y"]].set_index("cluster_id")
+
+OLD_LOCS = pd.read_csv(os.path.join(DATA_DIR, "locations.csv"))
+
+def filter_data(df: pd.DataFrame, filters: dict):
+    ret_df = df.copy()
+    for k, v in filters.items():
+        print(k, v)
+        if v == "All":
+            continue
+        ret_df = ret_df.loc[ret_df[k] == v]
+    return ret_df
+
+    
+
 class MapPlot:
 
     def __init__(self) -> None:
         self.data_dir = DATA_DIR
-        # self.raw_df =
-    
+
     @APP.callback(
         dash.dependencies.Output("map_graph", 'figure'),
         [dash.dependencies.Input("trigger_plot", "n_clicks")],
-        [dash.dependencies.State("waste_class", 'value'), dash.dependencies.State("city", 'value')]
+        [dash.dependencies.State("waste_class", 'value'), dash.dependencies.State("city", 'value'), dash.dependencies.State("Date", "date"), dash.dependencies.State("date_offset", "value"), dash.dependencies.State("is_green", "value")]
     )
     def update_graph(*args):
         print("Updating graph..")
@@ -122,13 +161,82 @@ class MapPlot:
         loc_info = ast.literal_eval(args[2])
         default_frame = {'lat': loc_info[0], 'lon':loc_info[1]}
         default_zoom = loc_info[2]
+        curr_date = pd.Timestamp(args[3]).date()
+        date_offset = rd(days=30-args[4]-1)
+        curr_date = curr_date - date_offset
+        curr_date = f"{curr_date:%d.%m.%y}"
+        is_green = args[5]
+        filters = {'NAZIV_ODPADKA_MERGED': waste_class, "DAT_ODDAJA": curr_date}
 
-        #TODO: ADD FILTERS FOR DF ACCORING TO TEXT FIELDS
-        df = RAW_DF.copy()
-        # breakpoint()
-        # fig = px.scatter_mapbox(df, mapbox_style="stamen-terrain", lon='lon', lat='lat', animation_frame='year', size='pop', center=default_frame, zoom=default_zoom)
-        fig = px.scatter_mapbox(df, mapbox_style="stamen-terrain", lon='lon', lat='lat', center=default_frame, zoom=default_zoom, hover_name="Lokacija")
+        if len(is_green) == 0:
+            t_df = OLD_LOCS.copy()
+            fig = px.scatter_mapbox(t_df, mapbox_style="stamen-terrain", lon='lon', lat='lat', center=default_frame, zoom=default_zoom, hover_name="Lokacija")
+            fig.update_layout(width = 1750, height=850)
+            return fig
+
+        ## SCATTER ##
+        df = filter_data(RAW_DF_CLUS, filters)
+        
+        df_out = df.groupby(['DB_CLUSTER_LOCATION_ODDAJA', ]).agg({"KOLIČINA ODPADKA v kg": "sum"}).rename({"KOLIČINA ODPADKA v kg":"kg"}, axis=1)
+        df_in = df.groupby(['DB_CLUSTER_LOCATION_PREJEMA', ]).agg({"KOLIČINA ODPADKA v kg": "sum"}).rename({"KOLIČINA ODPADKA v kg":"kg"}, axis=1)
+
+        df_net = df_in.join(df_out, how='outer', lsuffix = "_out", rsuffix="_in").fillna(0.0)
+        df_net['net_kg']  = df_net['kg_in'] - df_net['kg_out']
+        df_net['abs_net_kg']  = df_net['kg_in'] + df_net['kg_out']
+        df_net['abs_net_kg_log']  = np.log2(df_net["abs_net_kg"])
+
+        df_final = df_net.join(ALL_LOCS, how='inner')
+
+        ## LINES ##
+        df_lines = filter_data(RAW_DF_CLUS, filters).groupby(["DB_CLUSTER_LOCATION_ODDAJA", "DB_CLUSTER_LOCATION_PREJEMA"]).agg({"KOLIČINA ODPADKA v kg":"sum", "DB_CLUSTER_LOCATION_ODDAJA_X":"first", "DB_CLUSTER_LOCATION_ODDAJA_Y":"first", "DB_CLUSTER_LOCATION_PREJEMA_X":"first", "DB_CLUSTER_LOCATION_PREJEMA_Y":"first"}).rename({"DB_CLUSTER_LOCATION_ODDAJA_X":"X1", "DB_CLUSTER_LOCATION_ODDAJA_Y": "Y1", "DB_CLUSTER_LOCATION_PREJEMA_X":"X2", "DB_CLUSTER_LOCATION_PREJEMA_Y":"Y2", "KOLIČINA ODPADKA v kg":"kg"}, axis=1)
+        fig_map = px.scatter_mapbox(df_final, mapbox_style="stamen-terrain", lon='Y', lat='X',color='net_kg' , size='abs_net_kg_log')
+    
+
+        fig_lines = go.Figure()
+        for i in range(len(df_lines)):
+            fig_lines.add_trace(
+                go.Scattermapbox(
+                    lat = [list(df_lines['X1'])[i], list(df_lines['X2'])[i]],
+                    lon = [list(df_lines['Y1'])[i], list(df_lines['Y2'])[i]],
+                    mode = 'lines',
+                    line = dict(width=int(np.log10(list(df_lines['kg'])[i])), color='red'),
+                    # text =[f"Total kg.: {list(df_lines['kg'])[i]}"],
+                    # hoverinfo ='text'                    
+                    # opacity = float(df_flight_paths['cnt'][i]) / float(df_flight_paths['cnt'].max()),
+                )
+            )
+        
+        fig3 = go.Figure(data=fig_map.data + fig_lines.data, layout = { "colorscale": go.layout.Colorscale(diverging='solar')})
+        # fig3 = go.Figure(data= fig_lines.data, layout = { "colorscale": go.layout.Colorscale(diverging='solar')})
+        fig3.update_layout({"showlegend":False,"mapbox_style":"stamen-terrain"}, mapbox={'center':default_frame, 'zoom':default_zoom}, width = 1750, height=850)    
+        
+
+        return fig3
+
+    @APP.callback(  
+        dash.dependencies.Output("bar_graph", 'figure'),
+        [dash.dependencies.Input("trigger_plot", "n_clicks")],
+        [dash.dependencies.State("waste_class", 'value'), dash.dependencies.State("Date", "date"), dash.dependencies.State("date_offset", "value")]
+    )
+
+    def update_graph(*args):
+        print("Updating histogram..")
+
+        waste_class = args[1]
+        curr_date = pd.Timestamp(args[2]).date()
+        date_offset = rd(days=30-args[3]-1)
+        curr_date = curr_date - date_offset
+        curr_date = f"{curr_date:%d.%m.%y}"
+        filters = {"DAT_ODDAJA": curr_date}
+        df = filter_data(RAW_DF_CLUS, filters)
+        df = df.groupby("NAZIV_ODPADKA_MERGED").agg({"KOLIČINA ODPADKA v kg": "sum"}).rename({"KOLIČINA ODPADKA v kg":"kg"}, axis=1)
+        df['Waste type'] = df.index
+        # print(df)
+        
+        fig = px.bar(df, color='Waste type', labels={'NAZIV_ODPADKA_MERGED': "Waste type", "value": "Total kgs."}, template='plotly_white')
         fig.update_layout(width = 1750, height=850)
+    
+        print("Done")
         return fig
 
 
